1e678e2fe1c6fa00ea0ba6dc6288e795e4fa3e1b
Jevin Sweval <jevinsweval@gmail.com>
2025-03-19 16:19:07 -0400

    refactor tool

--- python/src/instdec/tools/instdec_util.py
+++ python/src/instdec/tools/instdec_util.py
@@ -8,19 +8,8 @@ from rich import print
 from instdec import arm_json
 
 
-def get_arg_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(prog="instdec-py-util")
-    parser.add_argument(
-        "-i", "--instr-json", dest="instr_json", required=True, help="Instructions.json path"
-    )
-    return parser
-
-
-def real_main(args: argparse.Namespace):
-    print(f"args: {args}")
-    j = dict(json.load(open(args.instr_json)))
-    instructions = arm_json.find_leafs(j)
-
+def dump_instructions_old(raw_json: dict | list) -> None:
+    instructions = arm_json.find_leafs(raw_json)
     for inst in instructions:
         name, mtrits, n0, n1, nX = arm_json.parse_instruction_encoding(inst)
         print(f"inst: {name:32} mtrits: {mtrits} #0: {n0:2} #1: {n1:2} #X: {nX:2}")
@@ -36,13 +25,27 @@ def real_main(args: argparse.Namespace):
         #     print("condition:")
         #     print(condition)
         #     sys.exit(0)
+    json.dump(instructions, open("inst-enc.json", "w"))
+
+
+def get_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="instdec-py-util")
+    parser.add_argument(
+        "-i", "--instr-json", dest="instr_json", required=True, help="Instructions.json path"
+    )
+    return parser
+
+
+def real_main(args: argparse.Namespace):
+    print(f"args: {args}")
+    raw_json = dict(json.load(open(args.instr_json)))
+
+    dump_instructions_old(raw_json)
 
     print(f"seen_identifiers: {arm_json.seen_identifiers}")
     print(f"seen_function_names: {arm_json.seen_function_names}")
     print(f"seen_value_meanings: {arm_json.seen_value_meanings}")
     print(f"seen_value_values: {arm_json.seen_value_values}")
-
-    json.dump(instructions, open("inst-enc.json", "w"))
 
 
 def main():
