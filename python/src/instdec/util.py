acc76d49eadb3188d59c31930bc6b78ecd28d1a7
Jevin Sweval <jevinsweval@gmail.com>
2025-03-22 16:52:06 -0400

    better

--- python/src/instdec/util.py
+++ python/src/instdec/util.py
@@ -45,7 +45,7 @@ def tag(tag_val: str) -> Callable[[CTB], CTB]:
             raise ValueError(f"cls should be a type not '{type(cls)}' cls: {cls}")
         if not issubclass(cls, TagBase):
             raise TypeError(f"cls not subclass of TagBase cls: {cls}")
-        setattr(cls, "_type", tag_val)
+        cls._type = tag_val
         return cls
 
     return wrap
