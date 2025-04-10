cff57b5cd95c990fa80ca58c4188042b7712ef2d
Jevin Sweval <jevinsweval@gmail.com>
2025-04-10 17:38:17 -0400

    instdec-util C++ skeleton

---
+++ tools/instdec-util/instdec-util.cpp
@@ -0,0 +1,14 @@
+#undef NDEBUG
+#include <cassert>
+
+#include <cstdint>
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fmt/std.h>
+#include <nlohmann/json.hpp>
+#include <scn/scan.h>
+
+int main(int argc, const char **argv) {
+    return 0;
+}
