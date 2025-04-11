486a78af73a2079bcadf3f1d202ea9cb1d9eb115
Jevin Sweval <jevinsweval@gmail.com>
2025-04-11 00:17:12 -0400

    it does... something

--- python/src/instdec/cpsat.py
+++ python/src/instdec/cpsat.py
@@ -1,14 +1,27 @@
+import pandas as pd
 from ortools.sat.python import cp_model
 from rich import print
 
 
 def cpsat_check_encoding(einf: dict[str, tuple[int, int]]) -> None:
     model = cp_model.CpModel()
-    ib = [model.new_bool_var(f"i{i}") for i in reversed(range(32))]
+    pidx = pd.Index(range(32), name="idx")
+    ib = model.new_bool_var_series("i", pidx)
+    # ib = [model.new_bool_var(f"i{i}") for i in reversed(range(32))]
+    ivs = {}
     for iname, ei in einf.items():
         bm, bp = ei
         iv = model.new_bool_var(f"v_{iname}")
-        model.add_bool_and(
-            [ib[i] == int(bool(bp & (1 << i))) for i in reversed(range(32)) if bm & (1 << i)]
-        ).only_enforce_if(iv)
+        ivs[iname] = iv
+        lits = []
+        for x in range(32):
+            lits.append(ib[int(x)] == True)  # noqa: E712
+
+        for i in list(range(32)):
+            if bm & (1 << i):
+                bpb = bool(bp & (1 << i))
+                lit = model.new_bool_var(f"v_{iname}_{int(bpb)}")
+                model.AddBoolAnd(lit).OnlyEnforceIf(iv)
+                # lits.append(lit)
+    model.add_at_least_one(ivs.values())
     print(model)
