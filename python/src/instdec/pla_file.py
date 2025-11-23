706b879efb961c62f208f2e6809041d1118ca022
Jevin Sweval <jevinsweval@gmail.com>
2025-11-23 18:11:34 -0500

    wip

--- python/src/instdec/pla_file.py
+++ python/src/instdec/pla_file.py
@@ -3,8 +3,15 @@ from __future__ import annotations
 import attrs
 
 
+@attrs.define(auto_attribs=True, frozen=True)
+class Term:
+    ins: str
+    outs: str
+
+
 @attrs.define(auto_attribs=True)
 class PLA:
+    terms: list[Term]
     num_in: int
     num_out: int
     labels_in: list[str] | None = None
@@ -13,6 +20,15 @@ class PLA:
 
     @staticmethod
     def from_str(pla_str: str) -> PLA:
+        lin: list[str] | None = None
+        lout: list[str] | None = None
+        t: list[Term] = []
+        n: str | None = None
+        for ln in pla_str.splitlines():
+            # match ln in
+            str(ln)
+            pass
+        map(str, (lin, lout, t, n))
         return PLA(0, 0)
 
     def to_str(self) -> str:
