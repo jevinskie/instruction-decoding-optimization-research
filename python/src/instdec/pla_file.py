732c8af4750aac4a01d0d02a7cf8667bf9b31e51
Jevin Sweval <jevinsweval@gmail.com>
2025-11-24 11:54:54 -0500

    .pla extensions

--- python/src/instdec/pla_file.py
+++ python/src/instdec/pla_file.py
@@ -16,42 +16,58 @@ _term_pattern = re.compile(r"([01-]+)\s+([01-]+)")
 
 @attrs.define(auto_attribs=True)
 class PLA:
+    # https://people.eecs.berkeley.edu/~alanmi/research/espresso/espresso_5.html
     terms: list[Term]
     num_in: int
     num_out: int
     labels_in: list[str] | None = None
     labels_out: list[str] | None = None
-    name: str | None = None
 
     @staticmethod
     def from_str(pla_str: str) -> PLA:
+        # this is non-compliant in many ways (e.g. treats whitespace as significant)
         t: list[Term] = []
         ni: int | None = None
         no: int | None = None
         lin: list[str] | None = None
         lout: list[str] | None = None
-        n: str | None = None
         expected_p: int | None = None
-        for ln in pla_str.splitlines():
+        for line_no, ln in enumerate(pla_str.splitlines()):
             if len(ln) == 0:
                 continue
             if ln.startswith("#"):
                 continue
             if ln.startswith("."):
                 cmd = ln.removeprefix(".")
-                match cmd:
-                    case "i":
-                        ni = int(cmd.removeprefix("i"))
-                    case "o":
-                        no = int(cmd.removeprefix("o"))
-                    case "ilb":
-                        lin = list(cmd.removeprefix("ilb").split())
-                    case "olb":
-                        lout = list(cmd.removeprefix("olb").split())
-                    case "p":
-                        expected_p = int(cmd.removeprefix("p"))
-                    case "e":
-                        break
+                if cmd.startswith("ilb "):
+                    if lin is not None:
+                        raise ValueError(".ilb already processed")
+                    lin = list(cmd.removeprefix("ilb ").split())
+                    continue
+                elif cmd.startswith("ob "):
+                    if lout is not None:
+                        raise ValueError(".ob already processed")
+                    lout = list(cmd.removeprefix("ob ").split())
+                    continue
+                elif cmd.startswith("i "):
+                    if ni is not None:
+                        raise ValueError(".i already processed")
+                    ni = int(cmd.removeprefix("i "))
+                    continue
+                elif cmd.startswith("o "):
+                    if no is not None:
+                        raise ValueError(".o already processed")
+                    no = int(cmd.removeprefix("o "))
+                    continue
+                elif cmd.startswith("p "):
+                    if expected_p is not None:
+                        raise ValueError(".p already processed")
+                    expected_p = int(cmd.removeprefix("p "))
+                    continue
+                elif cmd == "e":
+                    break
+                else:
+                    raise ValueError(f"unhandled line: line_no:{line_no:2} '{ln}'")
             term_matches = _term_pattern.match(ln)
             if term_matches:
                 in_terms_str, out_terms_str = term_matches.groups()
@@ -61,8 +77,13 @@ class PLA:
         if expected_p is not None and len(t) != expected_p:
             raise ValueError(f"Got {len(t)} terms not {expected_p} as specified by '.p N'")
         if len(t) == 0:
-            return PLA([], ni if ni is not None else 0, no if no is not None else 0, lin, lout, n)
-        return PLA(0, 0)
+            return PLA([], ni if ni is not None else 0, no if no is not None else 0, lin, lout)
+        calc_ni, calc_no = len(t[0].ins), len(t[0].outs)
+        if ni is None or calc_ni != ni:
+            raise ValueError(".i bad")
+        if no is None or calc_no != no:
+            raise ValueError(".o bad")
+        return PLA(t, ni, no, lin, lout)
 
     def to_str(self) -> str:
         return ""
--- python/tests/test_pla_file.py
+++ python/tests/test_pla_file.py
@@ -1,6 +1,98 @@
-import instdec.pla_file as pla_file
+from instdec.pla_file import PLA, Term
+
+four_bit_pal_test_a = r"""
+.i 4
+.o 1
+.ilb A B C D
+.ob MAJ
+.p 8
+0111 1
+1001 1
+1010 1
+1011 1
+1100 1
+1101 1
+1110 1
+1111 1
+.e
+"""
+
+four_bit_pal_test_b = r"""
+.i 4
+.o 1
+.ilb A B C D
+.ob MAJ
+.p 5
+0111 1
+1011 1
+1101 1
+1110 1
+1111 1
+.e
+"""
+
+four_bit_pal_test_c = r"""
+.i 4
+.o 1
+.ilb A B C D
+.ob MAJ
+.p 4
+-111 1
+11-- 1
+1-1- 1
+1--1 1
+.e
+"""
 
 
-def test_pla_file_load():
-    po = pla_file.PLA([], 0, 0)
-    assert po is not None
+def test_pla_file_load_a():
+    po = PLA.from_str(four_bit_pal_test_a)
+    assert po == PLA(
+        terms=[
+            Term(ins="0111", outs="1"),
+            Term(ins="1001", outs="1"),
+            Term(ins="1010", outs="1"),
+            Term(ins="1011", outs="1"),
+            Term(ins="1100", outs="1"),
+            Term(ins="1101", outs="1"),
+            Term(ins="1110", outs="1"),
+            Term(ins="1111", outs="1"),
+        ],
+        num_in=4,
+        num_out=1,
+        labels_in=["A", "B", "C", "D"],
+        labels_out=["MAJ"],
+    )
+
+
+def test_pla_file_load_b():
+    po = PLA.from_str(four_bit_pal_test_b)
+    assert po == PLA(
+        terms=[
+            Term(ins="0111", outs="1"),
+            Term(ins="1011", outs="1"),
+            Term(ins="1101", outs="1"),
+            Term(ins="1110", outs="1"),
+            Term(ins="1111", outs="1"),
+        ],
+        num_in=4,
+        num_out=1,
+        labels_in=["A", "B", "C", "D"],
+        labels_out=["MAJ"],
+    )
+
+
+def test_pla_file_load_c():
+    po = PLA.from_str(four_bit_pal_test_c)
+    assert po == PLA(
+        terms=[
+            Term(ins="-111", outs="1"),
+            Term(ins="11--", outs="1"),
+            Term(ins="1-1-", outs="1"),
+            Term(ins="1--1", outs="1"),
+        ],
+        num_in=4,
+        num_out=1,
+        labels_in=["A", "B", "C", "D"],
+        labels_out=["MAJ"],
+    )
